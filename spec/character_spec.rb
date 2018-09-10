5e4447ca9fba09f4794554778907700da4aa933d
Amit Singh <amit_nyc@live.com>
2018-09-10 17:33:07 +0000

    automatically backed up by learn

--- spec/character_spec.rb
+++ spec/character_spec.rb
@@ -27,9 +27,9 @@ describe Character do
 
     urkel = Character.new(:name => "Steve Urkel")
     urkel.catchphrase = "Did I do that?"
-    binding.pry
-    urkel.save
 
+    urkel.save
+    binding.pry
     expect(Character.find_by(:id => urkel.id).catchphrase).to eq(urkel.catchphrase)
 
     expect(urkel.say_that_thing_you_say).to eq("#{urkel.name} always says: #{urkel.catchphrase}")
