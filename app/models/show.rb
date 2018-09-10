ae63ab380357794ed66a267a67952fd5d6b74ae1
Amit Singh <amit_nyc@live.com>
2018-09-10 19:36:45 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,5 +1,6 @@
 class Show < ActiveRecord::Base
-  belongs_to :networks
   has_many :characters
   has_many :actors, through: :characters
+  has_many :networks
+  has_many :networks, through: :shows
 end
