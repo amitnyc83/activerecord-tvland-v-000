d92d3ab313732b8e89b3ee34b4fdf94ee37be257
Amit Singh <amit_nyc@live.com>
2018-09-10 18:50:34 +0000

    automatically backed up by learn

--- db/migrate/009_add_day_and_seasons_column_to_shows.rb
+++ db/migrate/009_add_day_and_seasons_column_to_shows.rb
@@ -1,8 +1,8 @@
-class AddDayAndSeasonsColumnToShows
+class AddDayAndSeasonsColumnToShows < ActiveRecord::Migration[4.2]
 
 def change
   add_column :shows, :day, :string
-  add_column :shows, :season, :string 
+  add_column :shows, :season, :string
 end
 
 end
